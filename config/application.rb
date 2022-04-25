9bf063922b9270754414d6b6cfd886f51ede5a53
k_yamada <offmailbox1225@gmail.com>
2022-04-25 17:28:01 +0000

    static_pagesコントローラーを追加

--- config/application.rb
+++ config/application.rb
@@ -10,8 +10,8 @@ module AttendanceApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-　  config.time_zone = 'Asia/Tokyo'
-
+    config.time_zone = 'Asia/Tokyo'
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
